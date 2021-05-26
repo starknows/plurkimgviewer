b60012e483775027295d206f55017e2025ed184d
wuxu1128 <xing255161@gmail.com>
2021-05-26 23:19:20 +0800

    0526 v3 u2/twitter

--- plurkImgContenScript.js
+++ plurkImgContenScript.js
@@ -1,6 +1,10 @@
 let timeout
 let bodyDom = $('body.timeline')
 let size = { width: null, height: null }
+let keyWord = [
+  { host: 'twitter', key: 'pbs.twimg' },
+  { host: 'youtube', key: 'i.ytimg' },
+]
 
 function reSetSize(size) {
   $('.plurkIMG-popup-wrapper')
@@ -37,41 +41,62 @@ $(bodyDom)
     <img src="">
     </figure>`
   )
-  .on('mouseenter', 'a.pictureservices > img', function (e) {
-    if (
-      $('.cbox_left .img-holder').length > 0 ||
-      e.target.src.indexOf('imgs.plurk')
-    )
-      return true
-    timeout && clearTimeout(timeout)
-    //imgs.plurk直接跳過
-    //i.ytimg >> default換成hqdefault
-    let src
-    if (!e.target.alt) {
-    } else {
-      src = e.target.alt
-    }
-
-    let place = e.pageX > $(window).width() / 2 ? 'left' : 'right'
-    let activeViewer = $('.plurkIMG-popup-wrapper.active')
-    if ($(activeViewer).length < 1) {
-      viewerAction(place)
-      activeViewer = $('.plurkIMG-popup-wrapper.active')
-    }
-    $(activeViewer)
-      .children('img')
-      .attr('src', src)
-      .on('load', function (e) {
-        e.target.style.filter = 'blur(0) drop-shadow(0 0 0.5rem #333)'
-      })
-  })
-  .on('mouseleave', 'a.pictureservices > img', function () {
-    $('.plurkIMG-popup-wrapper.active > img').css('filter', 'blur(10px)')
-    timeout = setTimeout(function () {
-      viewerAction(false)
-      $('.plurkIMG-popup-wrapper')
+  .on(
+    'mouseenter',
+    '.ex_link.pictureservices , .ex_link.meta > img, a.ogvideo > img',
+    function (e) {
+      if (
+        $('.cbox_left .img-holder').length > 0 ||
+        e.target.src.indexOf('imgs.plurk') > 0
+      ) {
+        return true
+      }
+      timeout && clearTimeout(timeout)
+      let src, host
+      if (!e.target.alt) {
+        for (let i = 0; i < keyWord.length; i++) {
+          if (e.target.src.indexOf(keyWord[i].key) > 0) {
+            host = keyWord[i].host
+            break
+          }
+        }
+        switch (host) {
+          case 'twitter':
+            src = e.target.src.replace(':large', ':medium')
+            break
+          case 'youtube':
+            src = e.target.src.replace('/default', '/sddefault')
+          default:
+            break
+        }
+      } else {
+        src = e.target.alt
+      }
+      let place = e.pageX > $(window).width() / 2 ? 'left' : 'right'
+      let activeViewer = $('.plurkIMG-popup-wrapper.active')
+      if ($(activeViewer).length < 1) {
+        viewerAction(place)
+        activeViewer = $('.plurkIMG-popup-wrapper.active')
+      }
+      $(activeViewer)
         .children('img')
-        .attr('src', chrome.runtime?.getURL('images/Loading.gif') || null)
-        .css('filter', 'blur(0px)')
-    }, 150)
-  })
+        .attr('src', src)
+        .on('load', function (e) {
+          e.target.style.filter = 'blur(0) drop-shadow(0 0 0.5rem #333)'
+        })
+    }
+  )
+  .on(
+    'mouseleave',
+    '.ex_link.pictureservices , .ex_link.meta > img, a.ogvideo > img',
+    function () {
+      $('.plurkIMG-popup-wrapper.active > img').css('filter', 'blur(10px)')
+      timeout = setTimeout(function () {
+        viewerAction(false)
+        $('.plurkIMG-popup-wrapper')
+          .children('img')
+          .attr('src', chrome.runtime?.getURL('images/Loading.gif') || null)
+          .css('filter', 'blur(0px)')
+      }, 150)
+    }
+  )
